e59fc1a927eae8505765547a9302afb85333f34c
Shuo Chen <chenatu2006@gmail.com>
2016-07-27 08:31:09 +0800

    fix bug of selector

--- src/main/resources/static/common.js
+++ src/main/resources/static/common.js
@@ -1,6 +1,6 @@
 $(function() {
     $("nav div ul li").each(function(){
-        if($(this).attr("id").includes($('body').attr("id")))
+        if(typeof($(this).attr("id")) != "undefined" && $(this).attr("id").includes($('body').attr("id")))
             $(this).addClass("active");
     })
 });
